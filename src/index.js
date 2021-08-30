e6d47a2f170e77c1ef8003bfe938940c183a9f45
Christerle_Nounoute <christerlesn@gmail.com>
2021-08-29 19:02:10 -0700

    created post request. Renamed a function to render. Dry up data

--- src/index.js
+++ src/index.js
@@ -2,25 +2,52 @@ const endPoint = "http://localhost:3000/api/v1/ideas"
 
 document.addEventListener('DOMContentLoaded', () => {
     getIdeas()
+
+const createIdeaForm = document.querySelector("#create-idea-form")
+createIdeaForm.addEventListener("submit", (e) =>  createFormHandler(e))
+
 })
 
 function getIdeas(){
     fetch(endPoint)
     .then(response => response.json())
     .then(ideas => {
-        iteratedIdeas(ideas)
+        ideas.data.forEach(idea => {
+        render(idea)
+        })
     })
 }
 
-function iteratedIdeas(ideas){
-    ideas.data.forEach(idea => {
-        const ideaMarkup = `
-        <div data-id= ${idea.id}>
-            <h3>${idea.attributes.title}</h3>
-            <p>${idea.attributes.description}</p>
-            <p>${idea.attributes.category.name}</p>
-        </div>
+function render(idea){
+    const ideaMarkup = `
+    <div data-id= ${idea.id}>
+        <h3>${idea.attributes.title}</h3>
+        <p>${idea.attributes.description}</p>
+        <p>${idea.attributes.category.name}</p>
+    </div>
         <br></br>`
     document.querySelector('#idea-container').innerHTML += ideaMarkup
+}
+
+function createFormHandler(e){
+    e.preventDefault()
+    const titleInput = document.querySelector('#input-title').value
+    const descriptionInput = document.querySelector('#input-description').value
+    const categoryId = parseInt(document.querySelector('#categories').value)
+    postFetch(titleInput, descriptionInput, categoryId)
+}
+
+function postFetch(title, description, category_id){
+    // console.log(title, description, category_id)
+    let bodyData = {title, description, category_id}
+    fetch (endPoint, {
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body: JSON.stringify(bodyData)
+    })
+    .then(response => response.json())
+    .then(idea => {
+        console.log(idea)
+        render(idea)
     })
 }
